7ded4cbb858fa0c830d54d109774394f64f4bbce
Mimieam <t.echismimi@gmail.com>
2020-06-01 14:00:59 -0500

    peek_fn basic implementation working

--- app/scripts/libs/contextMenu.js
+++ app/scripts/libs/contextMenu.js
@@ -7,21 +7,27 @@
 import WS_MANAGER, { ws, formatId, formatName } from './neoWorkSpace'
 import browser from 'webextension-polyfill';
 
+window.WS_MANAGER = WS_MANAGER
 
+const interleave = ([ x, ...xs ], ys = []) =>
+  x === undefined
+    ? ys                             // base: no x
+    : [ x, ...interleave (ys, xs) ]  // inductive: some x
 
-const genCtxMenuEntry = ({ _ws = {}, ctx=null, parentId=null, withSubmenu=false, id=null, title=null}) => {
+const genCtxMenuEntry = ({ _ws = {}, ctx=null, parentId=null, act=true, withSubmenu=false, id=null, title=null}) => {
 // const genCtxMenuEntry = (_ws, ctx, parentId = null, withSubmenu = false) => {
   // console.log(_ws)
   const _id = formatId(_ws?.name || id)
-  const _name = formatName(_ws?.name || title)
-
+  const _name = formatName(_ws?.name || title )
+  // console.log(_id, _name)
   return {
     id: _id,
     title: _name,
-    act: (info, tab) => { console.log(`Menu component ${ _id }`) },
+    ...(act && {act: (info, tab) => { console.log(`Menu component ${ _id }`) }}),
     ...(withSubmenu && { menu: generateUniqueSubMenu(WORKSPACEMENU, _id) }),
     ...(ctx && {contexts: ctx}),
-    ...(parentId && {parentId: parentId}),
+    parentId: parentId,
+    // ...(parentId && {parentId: parentId}),
     ...(_ws && {_ws: _ws})
   }
 }
@@ -29,8 +35,12 @@ const genCtxMenuEntry = ({ _ws = {}, ctx=null, parentId=null, withSubmenu=false,
 const create_workspace_handler = async (info, tab) => {
   
   const wsName = window.prompt("Please Enter a Name for your new WorkSpace");
-  const win = await browser.windows.getCurrent({ populate: true })  
-  const aNewWorkSpace = ws.fromWindows([win], wsName)
+  // const win = await browser.windows.getLastFocused({ populate: true })  
+  // const aNewWorkSpace = ws.fromWindows([win], wsName)
+  const win = await browser.windows.getAll({populate:true})
+  const aNewWorkSpace = ws.fromWindows(win, wsName)
+  console.log(aNewWorkSpace, win)
+  WS_MANAGER.add(aNewWorkSpace)
 
   // const _id = formatId(aNewWorkSpace.name)
   // const _name = formatName(aNewWorkSpace.name)
@@ -38,13 +48,13 @@ const create_workspace_handler = async (info, tab) => {
   let menuComponent = genCtxMenuEntry({_ws: aNewWorkSpace, ctx:null, parentId:null, withSubmenu:true})
   dynamicRootMenuWorkSpace.push(menuComponent)
   
-  WS_MANAGER.add(aNewWorkSpace)
+  console.log(WS_MANAGER)
   CTX_MENU.updateMenu()
 
   return aNewWorkSpace
 }
 
-const delete_workspace_handler = async (info, tab) => {
+const delete_workspace_handler = (info, tab) => {
   console.log(`${ info.menuItemId } Clicked`)
   
   const wsName = window.prompt("Enter the name of a WorkSpace to DELETE")
@@ -62,7 +72,7 @@ const CONTEXTS = ["page", "frame", "selection", "link", "editable", "image", "vi
 const WORKSPACEMENU = [
   {
     id: 'add_fn', title: 'Add this tab', act: (info, tab) => {
-      console.log(WS_MANAGER, info, tab)
+      // console.log(WS_MANAGER, info, tab)
       WS_MANAGER.all[info.parentMenuItemId].add(info.pageUrl)
       CTX_MENU.updateMenu()
     }
@@ -82,16 +92,37 @@ const WORKSPACEMENU = [
 
 // menu element need to have Unique IDs
 const generateUniqueSubMenu = (m, parentName) => {
-  // WS_MANAGER.
+  console.log("ws_m", parentName, WS_MANAGER.get(parentName))
+  const arr1 = WS_MANAGER.get(parentName).windows.map(x => x.tabs.map(t => t.url))
+  const arr2 = Array.from({ length: arr1.length - 1 }, () => '|') 
+  const arr3 = interleave(arr1, arr2).flat().map(x => {
+    if (x == '|') {
+      return[ {
+        type: "separator"
+      }, {
+        type: "separator"
+      }]
+    }
+    return {
+      id: `${Math.random(1) + 100}`,
+      name: x,
+      title: x
+    }
+  }).flat()
+  console.log(arr3)
+
   return m.map(item => {
     return {
       ...item,
       ...{ id: `${ item.id }_${ formatId(parentName) }` },
       ...(item.id == 'peek_fn' && {
-        menu: [{
-          id: `${Math.random(1) + 100}`,
-          title: `${Math.random(1) + 100}_${parentName}`,
-      }] }),
+        menu:arr3
+        // menu: [{
+        //   id: `${Math.random(1) + 100}`,
+        //   title: `${Math.random(1) + 100}_${parentName}`,
+      
+        // }]
+      }),
     }
   })
 }
@@ -153,11 +184,12 @@ export class ctxMenu {
   }
 
   createMenu(menu, root = null) {
-    // console.log(menu)
+    console.log(menu, root)
     
     for (let _m of menu) {
-      let { id, title, menu, act, _ws } = _m
-      console.log(_m, `${formatName(title)} ${_ws?`(${_ws.tabCount})`:''}`)
+      let { id, title, menu, act, type, _ws } = _m
+      // console.log( id, title, menu, _m)
+      // console.log(_m, `${formatName(title)} ${_ws?`(${_ws.tabCount})`:''}`)
       // let _ws_object = null
       // // let tabs = []
       // if (id in WS_MANAGER.all) {
@@ -171,22 +203,44 @@ export class ctxMenu {
         // console.log(`create -> [Del] - exiting id: ${id}`)
         this.deleteMenu(id)
       }
-
+      // let res = genCtxMenuEntry({
+      //   id: id,
+      //   title: formatName(title),
+      //   ctx: ctxMenu.contexts,
+      //   parentId: root,
+      //   act: false,
+      //   _ws: false,
+      //   // ctx: null, parentId: null, withSubmenu: true
+      // })
+      // console.log(res,         {
+      //   id: id,
+      //   // title: `${formatName(title)}`,
+      //   title: `${formatName(title)} ${_ws?`(${_ws.tabCount})`:''}`.trim(),
+      //   contexts: ctxMenu.contexts,
+      //   parentId: root
+      //   })
       chrome.contextMenus.create(
-        // genCtxMenuEntry({
-        //   id: id,
-        //   title: formatName(title),
-        //   contexts: ctxMenu.contexts,
-        //   parentId: root
-        //   // ctx: null, parentId: null, withSubmenu: true
-        // })
-        {
-        id: id,
-        // title: `${formatName(title)}`,
-        title: `${formatName(title)} ${_ws?`(${_ws.tabCount})`:''}`.trim(),
-        contexts: ctxMenu.contexts,
-        parentId: root
-        }
+        
+        (type == "separator") ? {
+          type: "separator",
+          parentId: root,
+        }: 
+        genCtxMenuEntry({
+          id: id,
+          ...(title && {title: `${formatName(title)} ${_ws?`(${_ws.tabCount})`:''}`.trim()}),
+          ctx: ctxMenu.contexts,
+          parentId: root,
+          act: false,
+          _ws: false,
+          // ctx: null, parentId: null, withSubmenu: true
+        })
+        // {
+        // id: id,
+        // // title: `${formatName(title)}`,
+        // title: `${formatName(title)} ${_ws?`(${_ws.tabCount})`:''}`.trim(),
+        // contexts: ctxMenu.contexts,
+        // parentId: root
+        // }
       );
         
       if (act) { ctxMenu.listeners[id] = act }
