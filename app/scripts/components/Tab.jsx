fd1a09e46162b00e7d2ca423b213c5486c484444
Mimieam <t.echismimi@gmail.com>
2020-09-17 08:44:38 -0500

    prototype 1 - done

--- app/scripts/components/Tab.jsx
+++ app/scripts/components/Tab.jsx
@@ -2,9 +2,12 @@ import React, { useState, useEffect, Component } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import browser from 'webextension-polyfill';
 import { getItems, reorder, getItemStyle,getListStyle ,move } from './helpers'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClone, faThumbtack, faWindowClose } from '@fortawesome/free-solid-svg-icons'
+
 
 import useGlobal from './store';
-
+import './tab.css'
 
 export const FavIcon = ({ url }) => {
   const _style = {
@@ -15,7 +18,34 @@ export const FavIcon = ({ url }) => {
   }
   return <div style={ _style }> </div>
 }
+const ButtonStrip = (props) => {
+  const _style = {
+    marginLeft: "auto",
+    // width: "5em",
+    display: "flex",
+    justifyContent: "space-between",
+  }
 
+  const _fa_extra = {
+    filter: "drop-shadow(0 0 2px #000)"
+  }
+
+  return (
+    <div
+      style={ _style }
+    >
+      <div className={"square_btn"}>
+        <FontAwesomeIcon icon={faThumbtack} size="lg"/>
+      </div>
+      <div className={"square_btn"}>
+      <FontAwesomeIcon icon={faClone} size="lg"/>
+      </div>
+      <div className={"square_btn"}>
+      <FontAwesomeIcon icon={faWindowClose} size="lg"/>
+      </div>
+    </div>
+  )
+}
 export const Tab_ = ({ item, state, setState, index, ind }) => {
 
   const [globalState, globalActions] = useGlobal();
@@ -40,24 +70,15 @@ export const Tab_ = ({ item, state, setState, index, ind }) => {
       <div style={ _style_title }>
         { `${item.id}|${item.index}`} { item.title }
       </div>
+
+      <ButtonStrip/>
       {/* <button
-        type="button"
-        onClick={ () => {
-          const newState = [...state];
-          newState[ind].splice(index, 1);
-          setState(newState.filter(group => group.length)
-          );
-        } }
-      >
-        delete
-      </button> */}
-      <button
         style={_style_buttons}
         type="button"
         onClick={ () => globalActions.addToCounter(1) }
       >
         +1 to global
-      </button>
+      </button> */}
     </div>
   )
 }
